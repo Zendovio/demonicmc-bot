ed1fca3d5c0084940b339562832664a51ac5d6c7
Zendovo <33803331+Zendovo@users.noreply.github.com>
2018-03-19 13:24:56 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+const botconfig = require("./botconfig.json");
+const Discord = require("discord.js");
+const bot = new Discord.Client({disableEveryone: true});
+const fs = require("fs");
+const request = require("request");
+var rn = require('random-number');
+bot.commands = new Discord.Collection();
+var randomstring = require("randomstring");
+
+fs.readdir("./commands/", (err, files) => {
+
+  if (err) console.log(err);
+
+  let jsfile = files.filter(f => f.split(".").pop() === "js")
+  if (jsfile.length <= 0){
+    console.log("Couldn't find commands.")
+    return;
+  }
+
+  jsfile.forEach((f, i) => {
+    let props = require(`./commands/${f}`);
+    console.log(`${f} loaded!`);
+    bot.commands.set(props.help.name, props);
+  });
+
+});
+
+//Bot turns on
+// bot.on("ready", async () => {
+//   console.log(`${bot.user.username} is online!`);
+//   bot.user.setActivity("d!help");
+// });
+
+bot.on("message", async message => {
+  if (message.author.bot) return;
+  if (message.channel.type === "dm") return;
+
+  let prefix = botconfig.prefix;
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+
+  let commandfile = bot.commands.get(cmd.slice(prefix.length));
+  if(commandfile) commandfile.run(bot,message,args,prefix,request);
+});
+
+bot.login(botconfig.token);
